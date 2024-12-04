bafd8b327dec5d0e9b1d2a71a061f1276d3ee7d4
Gurwinder Singh <vargwin@gmail.com>
2024-12-04 20:06:25 +0530

    refactor

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -1,82 +1,52 @@
-fn is_match_at_index(
-    grid: &[&[u8]],
-    search: &[u8],
-    mut i: isize,
-    mut j: isize,
-    dir_i: isize,
-    dir_j: isize,
-) -> bool {
-    let mut c = 0;
-    let rows = grid.len() as isize;
-    let cols = grid[0].len() as isize;
-
-    while c < search.len() && i >= 0 && i < rows && j >= 0 && j < cols {
-        if grid[i as usize][j as usize] != search[c] {
-            return false;
-        }
-        i += dir_i;
-        j += dir_j;
-        c += 1;
-    }
-
-    c == search.len()
-}
-
-pub fn part_1(input: &str) -> u32 {
+pub fn part_1(input: &str) -> usize {
     let grid = input.lines().map(|x| x.as_bytes()).collect::<Vec<_>>();
     let mut count = 0;
     let rows = grid.len() as isize;
     let cols = grid[0].len() as isize;
+
     for i in 0..rows {
         for j in 0..cols {
-            if is_match_at_index(&grid, b"XMAS", i, j, 1, 0) {
-                count += 1;
-            }
-            if is_match_at_index(&grid, b"XMAS", i, j, 0, 1) {
-                count += 1;
-            }
-            if is_match_at_index(&grid, b"XMAS", i, j, 1, 1) {
-                count += 1;
-            }
-            if is_match_at_index(&grid, b"XMAS", i, j, 1, -1) {
-                count += 1;
-            }
-            if is_match_at_index(&grid, b"XMAS", i, j, -1, 0) {
-                count += 1;
-            }
-            if is_match_at_index(&grid, b"XMAS", i, j, 0, -1) {
-                count += 1;
-            }
-            if is_match_at_index(&grid, b"XMAS", i, j, -1, -1) {
-                count += 1;
-            }
-            if is_match_at_index(&grid, b"XMAS", i, j, -1, 1) {
-                count += 1;
-            }
+            let strings = [(0, 1), (1, 0), (1, 1), (1, -1)].iter().map(|(x, y)| {
+                (0..=3)
+                    .map_while(|p| {
+                        let i = (i + p * x) as usize;
+                        let j = (j + p * y) as usize;
+                        grid.get(i).and_then(|row| row.get(j)).copied()
+                    })
+                    .collect::<Vec<_>>()
+            });
+
+            count += strings
+                .filter(|x| matches!(&x[..], b"XMAS" | b"SAMX"))
+                .count();
         }
     }
 
     count
 }
 
-fn is_match_at_index_part_2(grid: &[&[u8]], i: usize, j: usize) -> bool {
-    let i = i as isize;
-    let j = j as isize;
-    let tl = is_match_at_index(grid, b"MAS", i - 1, j - 1, 1, 1);
-    let tr = is_match_at_index(grid, b"MAS", i - 1, j + 1, 1, -1);
-    let bl = is_match_at_index(grid, b"MAS", i + 1, j - 1, -1, 1);
-    let br = is_match_at_index(grid, b"MAS", i + 1, j + 1, -1, -1);
-    tl && tr || tr && br || br && bl || bl && tl
-}
-
 pub fn part_2(input: &str) -> u32 {
     let grid = input.lines().map(|x| x.as_bytes()).collect::<Vec<_>>();
     let mut count = 0;
-    let rows = grid.len();
-    let cols = grid[0].len();
+    let rows = grid.len() as isize;
+    let cols = grid[0].len() as isize;
+
     for i in 0..rows {
         for j in 0..cols {
-            if is_match_at_index_part_2(&grid, i, j) {
+            let strings = [(1, 1), (1, -1)].iter().map(|(x, y)| {
+                (-1..=1)
+                    .map_while(|p| {
+                        let i = (i + p * x) as usize;
+                        let j = (j + p * y) as usize;
+                        grid.get(i).and_then(|row| row.get(j)).copied()
+                    })
+                    .collect::<Vec<_>>()
+            });
+            if strings
+                .filter(|x| matches!(&x[..], b"MAS" | b"SAM"))
+                .count()
+                == 2
+            {
                 count += 1;
             }
         }
