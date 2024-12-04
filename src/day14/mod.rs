7241dd0527fd9bf47e5c08d736806879e0a97e50
Gurwinder Singh <vargwin@gmail.com>
2024-12-04 11:21:56 +0530

    boilerplate

---
+++ src/day14/mod.rs
@@ -0,0 +1,31 @@
+pub fn part_1(input: &str) -> u32 {
+    todo!()
+}
+
+pub fn part_2(input: &str) -> u32 {
+    todo!()
+}
+
+#[test]
+fn run_part_1() {
+    let input = include_str!("input.txt");
+    println!("Result: {}", part_1(input));
+}
+
+#[test]
+fn run_part_2() {
+    let input = include_str!("input.txt");
+    println!("Result: {}", part_2(input));
+}
+
+#[test]
+fn test_part_1() {
+    let data = r"";
+    assert_eq!(part_1(data), 18);
+}
+
+#[test]
+fn test_part_2() {
+    let data = r"";
+    assert_eq!(part_2(data), 9);
+}
