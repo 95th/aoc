5e079157bfa16466306fae8f45fbbc27d2788a38
Gurwinder Singh <vargwin@gmail.com>
2024-12-14 19:36:32 +0530

    ops

--- util/src/vector.rs
+++ util/src/vector.rs
@@ -45,3 +45,63 @@ impl std::ops::SubAssign for Vec2 {
         self.y -= other.y;
     }
 }
+
+impl std::ops::Add<i32> for Vec2 {
+    type Output = Self;
+
+    fn add(self, scalar: i32) -> Self {
+        Self::new(self.x + scalar, self.y + scalar)
+    }
+}
+
+impl std::ops::AddAssign<i32> for Vec2 {
+    fn add_assign(&mut self, scalar: i32) {
+        self.x += scalar;
+        self.y += scalar;
+    }
+}
+
+impl std::ops::Sub<i32> for Vec2 {
+    type Output = Self;
+
+    fn sub(self, scalar: i32) -> Self {
+        Self::new(self.x - scalar, self.y - scalar)
+    }
+}
+
+impl std::ops::SubAssign<i32> for Vec2 {
+    fn sub_assign(&mut self, scalar: i32) {
+        self.x -= scalar;
+        self.y -= scalar;
+    }
+}
+
+impl std::ops::Mul<i32> for Vec2 {
+    type Output = Self;
+
+    fn mul(self, scalar: i32) -> Self {
+        Self::new(self.x * scalar, self.y * scalar)
+    }
+}
+
+impl std::ops::MulAssign<i32> for Vec2 {
+    fn mul_assign(&mut self, scalar: i32) {
+        self.x *= scalar;
+        self.y *= scalar;
+    }
+}
+
+impl std::ops::Div<i32> for Vec2 {
+    type Output = Self;
+
+    fn div(self, scalar: i32) -> Self {
+        Self::new(self.x / scalar, self.y / scalar)
+    }
+}
+
+impl std::ops::DivAssign<i32> for Vec2 {
+    fn div_assign(&mut self, scalar: i32) {
+        self.x /= scalar;
+        self.y /= scalar;
+    }
+}
