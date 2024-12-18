557f159b618b052366a5b7094f0f7b670525e3f1
Gurwinder Singh <vargwin@gmail.com>
2024-12-18 21:47:15 +0530

    pq

--- 2024/src/16.rs
+++ 2024/src/16.rs
@@ -1,6 +1,21 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 use aoc_util::{Dir, Grid};
+
+#[derive(PartialEq, Eq)]
+struct Sort<T>(usize, T);
+
+impl<T: Eq> PartialOrd for Sort<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T: Eq> Ord for Sort<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.0.cmp(&self.0)
+    }
+}
 
 fn main() {
     let input = include_str!("../input/16.txt");
@@ -11,73 +26,68 @@ fn main() {
 fn part_1(input: &str) -> usize {
     let grid = Grid::from_bytes(input);
     let start = grid.find(|&c| c == b'S').unwrap();
+    let mut visited = grid.with_fill([false; 4]);
 
-    let mut points = usize::MAX;
-    let mut visited = grid.with_fill(usize::MAX);
-    let mut queue = VecDeque::new();
-    queue.push_back((start, Dir::Right, 0));
+    let mut min_dist = usize::MAX;
+    let mut queue = BinaryHeap::new();
+    queue.push(Sort(0, (start, Dir::Right)));
 
-    while let Some((pos, dir, so_far)) = queue.pop_front() {
-        if visited[pos] < so_far || grid[pos] == b'#' {
-            continue;
-        }
+    while let Some(Sort(dist, (pos, dir))) = queue.pop() {
         if grid[pos] == b'E' {
-            visited[pos] = visited[pos].min(so_far);
-            points = points.min(so_far);
+            min_dist = min_dist.min(dist);
             continue;
         }
-        visited[pos] = visited[pos].min(so_far);
-        let mut enqueue = |dir: Dir, cost: usize| {
-            let next = pos.neighbor(dir);
-            if grid[next] != b'#' {
-                queue.push_back((next, dir, so_far + cost));
-            }
-        };
-        enqueue(dir, 1);
-        enqueue(dir.turn_left(), 1001);
-        enqueue(dir.turn_right(), 1001);
+        if visited[pos][dir as usize] {
+            continue;
+        }
+        visited[pos][dir as usize] = true;
+        let next = pos.neighbor(dir);
+        if grid[next] != b'#' {
+            queue.push(Sort(dist + 1, (next, dir)));
+        }
+        queue.push(Sort(dist + 1000, (pos, dir.turn_left())));
+        queue.push(Sort(dist + 1000, (pos, dir.turn_right())));
     }
 
-    points
+    min_dist
 }
 
 fn part_2(input: &str) -> usize {
     let grid = Grid::from_bytes(input);
     let start = grid.find(|&c| c == b'S').unwrap();
 
-    let mut points = usize::MAX;
+    let mut min_dist = usize::MAX;
     let mut visited = grid.with_fill([usize::MAX; 4]);
-    let mut queue = VecDeque::new();
-    queue.push_back((start, Dir::Right, 0, Vec::new()));
     let mut path_map = HashMap::new();
 
-    while let Some((pos, dir, so_far, mut path)) = queue.pop_front() {
-        if visited[pos][dir as usize] < so_far || grid[pos] == b'#' {
+    let mut queue = BinaryHeap::new();
+    queue.push(Sort(0, (start, Dir::Right, vec![start])));
+
+    while let Some(Sort(dist, (pos, dir, path))) = queue.pop() {
+        if visited[pos][dir as usize] < dist {
             continue;
         }
-        path.push(pos);
+        visited[pos][dir as usize] = dist;
         if grid[pos] == b'E' {
-            visited[pos][dir as usize] = visited[pos][dir as usize].min(so_far);
-            points = points.min(so_far);
+            min_dist = min_dist.min(dist);
             path_map
-                .entry(so_far)
+                .entry(dist)
                 .or_insert_with(HashSet::new)
                 .extend(path);
             continue;
         }
-        visited[pos][dir as usize] = visited[pos][dir as usize].min(so_far);
-        let mut enqueue = |dir: Dir, cost: usize| {
-            let next = pos.neighbor(dir);
-            if grid[next] != b'#' {
-                queue.push_back((next, dir, so_far + cost, path.clone()));
-            }
-        };
-        enqueue(dir, 1);
-        enqueue(dir.turn_left(), 1001);
-        enqueue(dir.turn_right(), 1001);
+
+        let next = pos.neighbor(dir);
+        if grid[next] != b'#' {
+            let mut path = path.clone();
+            path.push(next);
+            queue.push(Sort(dist + 1, (next, dir, path)));
+        }
+        queue.push(Sort(dist + 1000, (pos, dir.turn_left(), path.clone())));
+        queue.push(Sort(dist + 1000, (pos, dir.turn_right(), path)));
     }
 
-    path_map[&points].len()
+    path_map[&min_dist].len()
 }
 
 #[test]
