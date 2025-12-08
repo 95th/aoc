427557033d19f500da0bf4a1c0c8cc610040007d
Gurwinder Singh <vargwin@gmail.com>
2025-12-08 10:25:46 +0530

    day 8 init

---
+++ 2025/src/08.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let input = include_str!("../input/08.txt");
+    println!("Part 1: {}", part_1(input));
+    println!("Part 2: {}", part_2(input));
+}
+
+fn part_1(input: &str) -> i32 {
+    0
+}
+
+fn part_2(input: &str) -> i32 {
+    0
+}
+
+#[test]
+fn test_part_1() {
+    let data = r"";
+    assert_eq!(part_1(data), 0);
+}
+
+#[test]
+fn test_part_2() {
+    let data = r"";
+    assert_eq!(part_2(data), 0);
+}
